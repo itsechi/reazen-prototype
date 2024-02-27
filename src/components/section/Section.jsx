e94518c1f5954fb3083ff952198996ffb3d969ac
Oliwia <itsechi@gmail.com>
2024-02-27 17:07:53 +0100

    Add the first section

--- src/components/section/Section.jsx
+++ src/components/section/Section.jsx
@@ -1,3 +1,20 @@
+import styles from "./Section.module.scss";
+import { Link } from "../link/Link";
+
 export const Section = (props) => {
-  return <section></section>;
+  const buttons = props.buttons.map((button) => (
+    <Link className={button.style} href="#" text={button.text} />
+  ));
+
+  return (
+    <section className={styles.section}>
+      <div className={styles.sectionInfo}>
+        {props.header && <h1>{props.header}</h1>}
+        {props.subheader && <h2>{props.subheader}</h2>}
+        <p>{props.text}</p>
+        <div className={styles.sectionButtons}>{buttons}</div>
+      </div>
+      {props.carousel && <div className={styles.carousel}></div>}
+    </section>
+  );
 };
--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,10 +1,19 @@
 import { Section } from "../components/section/Section";
-import styles from './Main.module.scss';
+import styles from "./Main.module.scss";
 
 export const Main = () => {
   return (
     <main className={styles.main}>
-      <Section />
+      <div className={styles.mainBg}></div>
+      <Section
+        header="Supporting Human Artists"
+        text={`With the proliferation of AI-generated images, the art landscape is rapidly evolving. However, amidst this surge, we believe in championing the irreplaceable touch of human hands and the boundless imagination of artists. Each piece of artwork, every meticulously crafted merchandise, bears the soul of its creator, offering a glimpse into their unique perspective and passion.`}
+        carousel="true"
+        buttons={[
+          { text: "Support your favorite artist", style: "primary" },
+          { text: "Learn More", style: "secondary" },
+        ]}
+      />
     </main>
   );
 };
