5f6b32e0eadcbfba796343a8b0188602a07e7996
Oliwia <itsechi@gmail.com>
2024-02-29 14:54:51 +0100

    Add basic hover animations

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import "./App.css";
+import "./App.scss";
 import { Header } from "./components/header/Header";
 import { Main } from "./pages/Main";
 
--- src/components/section/Section.jsx
+++ src/components/section/Section.jsx
@@ -7,11 +7,18 @@ export const Section = (props) => {
     <Link key={i} className={button.style} href="#" text={button.text} />
   ));
 
+  // const images = [
+  //   "https://placehold.co/300x450/purple/white/?text=1",
+  //   "https://placehold.co/300x450/111B47/white/?text=2",
+  //   "https://placehold.co/300x450?text=3",
+  // ];
+
+  // just to test how things would look like
   const images = [
-    "https://placehold.co/300x450/purple/white/?text=1",
-    "https://placehold.co/300x450/111B47/white/?text=2",
-    "https://placehold.co/300x450?text=3",
-  ];
+    'https://ih1.redbubble.net/image.2025556950.2718/flat,750x1000,075,f.jpg',
+    'https://cdnb.artstation.com/p/assets/images/images/014/312/045/large/hlulani-brx-nukeri-dabi.jpg?1543439933',
+    'https://mir-s3-cdn-cf.behance.net/project_modules/hd/c3ce7c103847551.5f564f6a622e3.jpg'
+  ]
 
   return (
     <section
